c510e44073173b2d13ca83f332bfd617b92b020c
cwills7 <carl.r.wills@gmail.com>
2018-01-17 13:30:14 -0600

    EditEventArrayAdapter initial commit.

---
+++ app/src/main/java/com/wills/carl/socialworkhelper/edit/EditEventArrayAdapter.java
@@ -0,0 +1,10 @@
+package com.wills.carl.socialworkhelper.edit;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Carl on 1/16/2018.
+ */
+
+public class EditEventArrayAdapter extends ArrayAdapter {
+}
